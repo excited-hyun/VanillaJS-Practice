f7d55cdfc5fd79cd96496c303d9ec3dff5888e5a
excited-hyun <nahyun1127@naver.com>
2020-07-11 17:56:35 +0900

    VanillaJS DAY1

---
+++ VanillaJS_start1/script.js
@@ -0,0 +1,23 @@
+const nahyunInfo = {
+  name : "nahyun", 
+  age: 23,
+  sex: "F",
+  favMovies:["Avatar", "Interstellar"],
+  favFood:[
+    {
+      name: "pizza",
+      price: 25000
+    },
+    {
+      name: "chicken",
+      price: 18000
+    }
+  ]
+}
+
+const fruit= ["apple", "peach"]
+
+console.log(nahyunInfo)
+console.log(fruit)
+console.log(nahyunInfo.age)
+/* */
